e35b89ce6a06427a63868a4e950e9686654ead1f
AliiiBenn <davidddpereiraaa6@gmail.com>
2026-04-01 10:50:10 +0200

    feat: use Predicate/WhereNode type in database operations
    - Replace Partial<TData> with Predicate<TData> in FindManyQuery
    - Update findFirst, count, exists to use Predicate<T>
    - Database operations now use our type-safe where AST
    
    Co-Authored-By: martyy-code <nesalia.inc@gmail.com>

--- packages/collections/src/operations/database/types.ts
+++ packages/collections/src/operations/database/types.ts
@@ -1,9 +1,10 @@
 import type { Collection } from '../../collections'
 import type { InferFieldTypes } from '../../collections/hooks/types'
 import type { GetCollectionType } from '../../collections/types'
+import type { Predicate } from '../where'
 
 export interface FindManyQuery<TData> {
-  where?: Partial<TData>
+  where?: Predicate<TData>
   orderBy?: keyof TData
   limit?: number
   offset?: number
@@ -32,15 +33,15 @@ export type Counted<T> = T & { count: number }
 export type CollectionDbMethods<T extends Collection> = {
   findMany: (query?: FindManyQuery<InferFieldTypes<T['fields']>>) => Promise<GetCollectionType<T>[]>
   findUnique: (query: { where: WhereById }) => Promise<GetCollectionType<T> | null>
-  findFirst: (query?: { where: Partial<InferFieldTypes<T['fields']>>; orderBy?: keyof T['fields'] }) => Promise<GetCollectionType<T> | null>
+  findFirst: (query?: { where?: Predicate<InferFieldTypes<T['fields']>>; orderBy?: keyof T['fields'] }) => Promise<GetCollectionType<T> | null>
   create: (input: CreateOperation<InferFieldTypes<T['fields']>>) => Promise<GetCollectionType<T>>
   createMany: (input: CreateManyOperation<InferFieldTypes<T['fields']>>) => Promise<Counted<GetCollectionType<T>[]>>
   update: (input: { where: WhereById; data: Partial<InferFieldTypes<T['fields']>> }) => Promise<GetCollectionType<T>>
   updateById: (id: string, data: Partial<InferFieldTypes<T['fields']>>) => Promise<GetCollectionType<T>>
   delete: (input: { where: WhereById }) => Promise<GetCollectionType<T>>
   deleteById: (id: string) => Promise<GetCollectionType<T>>
-  count: (query?: { where?: Partial<InferFieldTypes<T['fields']>> }) => Promise<number>
-  exists: (query: { where: Partial<InferFieldTypes<T['fields']>> }) => Promise<boolean>
+  count: (query?: { where?: Predicate<InferFieldTypes<T['fields']>> }) => Promise<number>
+  exists: (query: { where: Predicate<InferFieldTypes<T['fields']>> }) => Promise<boolean>
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
